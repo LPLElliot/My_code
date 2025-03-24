acfa3fa9b9baef0a648994817c7374d4f027348c
ELLIOT <1371359665@qq.com>
2025-03-24 20:36:26 +0800

    小修改

--- c-c++/SqQueue_test.c
+++ c-c++/SqQueue_test.c
@@ -1,11 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define MaxSize 10
 
-typedef struct{
+typedef struct
+{
 	int *data;
-	int front,rear;
-}SqQueue;
+	int front, rear;
+} SqQueue;
 
-
+int main()
+{
+	SqQueue Q;
+	Q.data = (int *)malloc(MaxSize * sizeof(int));
+	Q.front = Q.rear = 0;
+	return 0;
+}
