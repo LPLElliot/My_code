c543b5e9974ec35a08304f9472db846361d7bcd1
ELLIOT <1371359665@qq.com>
2025-04-02 10:06:34 +0800

    修改配置

--- c-c++/leetcode/leetcode-2.c
+++ c-c++/leetcode/leetcode-2.c
@@ -16,14 +16,13 @@ struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
     ListNode *p = dummy;
     int carry = 0;
 
-    while (l1 != NULL || l2 != NULL)
+    while (l1 != NULL || l2 != NULL || carry)
     {
         ListNode *node = (ListNode *)malloc(sizeof(ListNode));
         node->val = carry + (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
         carry = node->val / 10;
         node->val %= 10;
 
-        // 将新节点接到末尾
         node->next = NULL;
         p->next = node;
         p = node;
@@ -33,14 +32,20 @@ struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
         if (l2)
             l2 = l2->next;
     }
-    if (carry > 0)
+    ListNode *head = dummy->next;
+    free(dummy);
+    return head;
+}
+
+void freeList(struct ListNode *head)
+{
+    struct ListNode *tmp;
+    while (head != NULL)
     {
-        ListNode *node = (ListNode *)malloc(sizeof(ListNode));
-        node->val = carry;
-        node->next = NULL;
-        p->next = node;
+        tmp = head;
+        head = head->next;
+        free(tmp);
     }
-    return dummy->next;
 }
 
 int main()
@@ -54,12 +59,15 @@ int main()
     l1->next->next->val = 9;
     l1->next->next->next = (struct ListNode *)malloc(sizeof(struct ListNode));
     l1->next->next->next->val = 9;
+    l1->next->next->next->next = NULL;
+
     l2->val = 9;
     l2->next = (struct ListNode *)malloc(sizeof(struct ListNode));
     l2->next->val = 9;
     l2->next->next = (struct ListNode *)malloc(sizeof(struct ListNode));
     l2->next->next->val = 9;
     l2->next->next->next = NULL;
+
     struct ListNode *result = addTwoNumbers(l1, l2);
     while (result != NULL)
     {
@@ -67,5 +75,9 @@ int main()
         result = result->next;
     }
     printf("\n");
+
+    freeList(l1);
+    freeList(l2);
+    freeList(result);
     return 0;
 }
