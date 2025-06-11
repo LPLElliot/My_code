89938cf46c488db9da1beeb8bcf774398916a705
张新建 <278254081@qq.com>
2025-06-11 19:48:00 +0800

    更新

--- c#/test2-Mvc/Models/PartyGuestViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace PartyApp.Models
-{
-    public class PartyGuestViewModel
-    {
-        public Party Party { get; set; } = new Party();
-        public List<Guest> Guests { get; set; } = new List<Guest>();
-    }
-}
